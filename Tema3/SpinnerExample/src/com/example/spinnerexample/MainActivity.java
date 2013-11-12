0907bfb57061afc7cdfb064b7192211011d13547
andressedano <andress@tid.es>
2013-11-12 22:36:07 +0100

    Spinner comment

--- Tema3/SpinnerExample/src/com/example/spinnerexample/MainActivity.java
+++ Tema3/SpinnerExample/src/com/example/spinnerexample/MainActivity.java
@@ -39,6 +39,9 @@ public class MainActivity extends Activity {
 				toast.show();
 			}
 
+			
+			// Cuando no hay opcion seleccionada, p.e por falta de datos en el
+			// adapter
 			public void onNothingSelected(AdapterView<?> parent) {
 				Toast toast = Toast.makeText(getApplicationContext(),
 						"Nothing Selected", duration);
